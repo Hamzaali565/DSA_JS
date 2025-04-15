5d3da13e01474546228dd1dd15617a50b614b7c8
Hamzaali565 <ha6153806@gmail.com>
2025-04-15 22:06:51 +0500

    traversing

---
+++ traversingAndAccessing.js
@@ -0,0 +1,10 @@
+// Traversing
+let data = [0, 1, 3, 45, 78, 97, 65, 443];
+
+for (i = 0; i < data?.length; i++) {
+  console.log(`Array ${i} element is ${data[i]}`);
+}
+//  for loop info
+// i = 0 ==> from where loop started
+// i < data?.lenght ==> at what index loop should iterate. Example if i < 4 then loop will iterate over 0 to 3 index
+// i++ forward looping increment in i on every iteration example after first iteration i=0 become 1=1 and so on.
