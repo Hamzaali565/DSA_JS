aafa968e75147590659a83158417038d2274c919
Hamzaali565 <ha6153806@gmail.com>
2025-04-15 22:09:25 +0500

    Accessing

--- traversingAndAccessing.js
+++ traversingAndAccessing.js
@@ -8,3 +8,8 @@ for (i = 0; i < data?.length; i++) {
 // i = 0 ==> from where loop started
 // i < data?.lenght ==> at what index loop should iterate. Example if i < 4 then loop will iterate over 0 to 3 index
 // i++ forward looping increment in i on every iteration example after first iteration i=0 become 1=1 and so on.
+
+// Accessing
+
+let el = [0, 1, 3, 45, 78, 97];
+console.log(el[0]); // 0
